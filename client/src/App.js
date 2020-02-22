6fbaa76e42e387e5a55a8896800867a92dca6e5b
Frederick Articuno <mckiyoshi@gmail.com>
2020-02-21 23:15:41 -0500

    copy pasta to win?

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
-//import logo from './logo.svg';
+import React, { Component } from 'react';
+import Form from './components/Form';
+import DisplayUsers from './components/DisplayUsers';
+import axios from 'axios';
 import './App.css';
-import Weather from './components/Weather/Weather'
-import Images from './components/Images/Images'
-function App() {
-  return (
-    <>
-    <div className="container">
-      <Weather />
-      <Images />
-    </div>
-    </>
-  );
+class App extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount = () => {
+    this.fetchUsers();
+  };
+
+  fetchUsers = () => {
+    axios.get('/users')
+      .then((response) => {
+        const { users } = response.data;
+        this.setState({ users: [...this.state.users, ...users] })
+      })
+      .catch(() => alert('Error fetching new users'));
+  };
+
+
+  addUser = ({ name, position, company }) => {
+    this.setState({
+      users: [...this.state.users, { name, position, company }]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Form addUser={this.addUser}/>
+        < DisplayUsers users={this.state.users} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
